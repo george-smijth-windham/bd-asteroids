299c7bf556388ffd620dcd72e8b76cfae5edc283
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-01-05 15:36:51 +0000

    update main.py to cap game loop to 60FPS

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ def main():
     )
     num_pass, num_fail = pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
@@ -21,6 +23,7 @@ def main():
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
