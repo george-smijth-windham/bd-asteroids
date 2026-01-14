a1ad8652398f48b0a6b007b37577cbea27ab1fac
George Smijth-Windham <gsmijthwindham+gh@pm.me>
2026-01-14 20:07:13 +0000

    update main.py to use groups instead of player

--- main.py
+++ main.py
@@ -16,21 +16,28 @@ def main():
     num_pass, num_fail = pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    running = True
     dt = 0
     # group = pygame.sprite.Group()
-    updatable, drawable = pygame.sprite.Group()
+    # updatable, drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
     player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
-    while True:
+    while running:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                running = False
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        # player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
+        # player.update(dt)
+        updatable.update(dt)
+    # pygame.quit()
 
 
 if __name__ == "__main__":
